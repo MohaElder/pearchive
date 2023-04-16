c6eff1e30e6b0767ff1461edca88fffc89974131
Yasushi Oh <yasushi@duck.com>
2023-04-16 21:41:15 +0700

    feat(PearFolder): Add support for containing PearFolder

--- src/libs/infrastructure/PearFolder.ts
+++ src/libs/infrastructure/PearFolder.ts
@@ -2,8 +2,8 @@ import type { PearFile } from "./PearFile";
 
 export class PearFolder {
     name : string;
-    files : PearFile[];
-    constructor(name : string, files : PearFile[]){
+    files : Array<PearFile | PearFolder>;
+    constructor(name : string, files : Array<PearFile | PearFolder>){
         this.name = name;
         this.files = files;
     }
